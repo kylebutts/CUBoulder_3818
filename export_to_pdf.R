af17e18a6612d9e5653db04a9942e1ea38ccc475
Kyle F. Butts <buttskyle96@gmail.com>
2021-09-27 09:06:18 -0600

    Reexported to pdf

--- export_to_pdf.R
+++ export_to_pdf.R
@@ -48,11 +48,8 @@ for(i in 1:length(rmd)) { print_rmd(rmd[i]) }
 # ---- Lecture Slides ----------------------------------------------------------
 
 rmd = list.files(here("Lecture Slides"), pattern = "*.Rmd", recursive = T)
-
-# Temporarily
-rmd = c("Chapter 01/ch1.Rmd", "Chapter 02/ch2.Rmd", "Chapter 12/ch12.Rmd", "Chapter 13/ch13.Rmd", "Chapter 14/ch14.Rmd", "Chapter 03/ch3.Rmd", "Distributions/distributions.Rmd", "Expectations/expectations.Rmd", "Chapter 08/ch8.Rmd", "Chapter 09/ch9.Rmd", "Chapter 15/ch15.Rmd", "Chapter 16/ch16.Rmd", "Chapter 17/ch17.Rmd", "Chapter 18/ch18.Rmd")
-
-# c("Chapter 04/ch4.Rmd", "Chapter 05/ch5.Rmd", "Chapter 06/ch6.Rmd", "Chapter 20/ch20.Rmd", "Chapter 21/ch21.Rmd", "Chapter 22/ch22.Rmd", "Chapter 23/ch23.Rmd", "Chapter 26/ch26.Rmd")
+# remove preamble.Rmd
+rmd = rmd[!stringr::str_detect(rmd, "preamble.Rmd")]
 
 rmd = here("Lecture Slides", rmd)
 pdf = stringr::str_replace(rmd, ".Rmd", ".pdf")
